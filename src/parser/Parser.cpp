03c4bb0a3a1e9d2cd6ebd05587dd94ae0f238853
Akul Singhal <singhal.akul9@gmail.com>
2021-04-13 23:30:29 +0530

    handle parse errors

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -14,13 +14,14 @@ ParseTree *parse_file(FILE *inputFile) {
     ParseTable parse_table = ParseTable("src/parser/parse_table");
     std::vector<GrammarRule> grammar_rules =
         grammarRules("src/parser/grammar_lalr.txt");
-
+    bool error_state = 0;
     while ((newToken = lexer.nextToken()) != NULL) {
         std::string token_element = newToken->toParseString();
         std::pair<char, int> operation =
             parse_table.transitions[stoi(st.top().first)][token_element];
         // std::cout << token_element << std::endl;
         while (operation.first == 'r') {
+            error_state = 0;
             int to_pop = 2 * grammar_rules[operation.second].rhs.size();
             std::vector<ParseTree *> children;
             for (int i = 0; i < to_pop; i++) {
@@ -43,10 +44,15 @@ ParseTree *parse_file(FILE *inputFile) {
                 parse_table.transitions[stoi(st.top().first)][token_element];
         }
         if (operation.first == 's') {
+            error_state = 0;
             st.push({token_element, new ParseTree(newToken, token_element)});
             st.push({std::to_string(operation.second), nullptr});
         } else {
-            throw "parse error";
+            if (error_state)
+                continue;
+            std::cerr << "Error Unexpected token: " << newToken->toString()
+                      << std::endl;
+            error_state = 1;
         }
     }
 
@@ -55,6 +61,7 @@ ParseTree *parse_file(FILE *inputFile) {
         parse_table.transitions[stoi(st.top().first)][token_element];
 
     while (operation.first == 'r') {
+        error_state = 0;
         int to_pop = 2 * grammar_rules[operation.second].rhs.size();
         std::vector<ParseTree *> children;
         for (int i = 0; i < to_pop; i++) {
@@ -77,6 +84,10 @@ ParseTree *parse_file(FILE *inputFile) {
             parse_table.transitions[stoi(st.top().first)][token_element];
     }
     if (operation.first == 'a') {
+        st.pop();
+        return st.top().second;
+    } else {
+        std::cerr << "Unsuccessful Parsing" << std::endl;
         st.pop();
         return st.top().second;
     }
